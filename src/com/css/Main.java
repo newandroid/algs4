012dc5e2df7aeb022c81e654ed3ed8cc7fcf2aaa
css <censhaoshuai2008@126.com>
2019-10-28 00:53:59 +0800

    test unit find api

--- src/com/css/Main.java
+++ src/com/css/Main.java
@@ -1,9 +1,15 @@
 package com.css;
 
+import com.css.algs4.UF;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("hello world");
+        UF uf = new UF(10);
+        uf.union(4, 3);
+        uf.union(3, 2);
+        System.out.println("find:" + uf.find(3));
+        System.out.println("connected:" + uf.connected(4, 3));
+        System.out.println("count:" + uf.count());
     }
 }
